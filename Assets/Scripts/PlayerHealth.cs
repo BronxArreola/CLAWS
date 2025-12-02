c742c25e42554631004e3de0e067df3016018957
Bronx <mba24a@acu.edu>
2025-12-01 21:51:23 -0600

    MORE STUFF

---
+++ Assets/Scripts/PlayerHealth.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerHealth : MonoBehaviour
+{
+    public int health;
+    public int maxHealth = 10;
+
+    public SpriteRenderer playerSr;
+    public SideScrollPlayer playerMovement;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        health = maxHealth;
+    }
+
+    public void TakeDamage(int amount)
+    {
+        health -= amount;
+        if(health <= 0)
+        {
+            playerSr.enabled = false;
+            playerMovement.enabled = false;
+        }
+    }
+}
